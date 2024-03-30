211c320738b181162e5353231834bc6a2904ed6f
Dorain <2394201457@qq.com>
2024-03-30 10:21:35 +0800

    删除菜品业务代码开发

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,9 +1,13 @@
 package com.sky.service;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 
 public interface DishService {
@@ -14,4 +18,17 @@ public interface DishService {
     @Transactional //事务注解
     public void  saveWitFlavor(DishDTO dishDTO);
 
+    /**
+     * 菜品分页查询
+     * @param dishPageQueryDTO
+     * @return
+     */
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
+
+    /**
+     *
+     * 菜品的批量删除功能
+     * @param ids
+     */
+    void deleteBatch(List<Long> ids);
 }
